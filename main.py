4d196b323f46c8a7b6bc14b0b4c2aeccb30b320a
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2023-10-04 13:18:54 +0300

    modified code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import AVLTree as avl
+import AvlTree as avl
 
 if __name__ == "__main__":
-    avl_tree = avl.AVLTree()
+    avl_tree = avl.AvlTree()
     root = avl_tree.insert(None, 0)
-    tree_values = [1, 2, 3, 4, 5, 6, 7]
+    tree_values = [1, 2, 3, 4]
     for number in tree_values:
         root = avl_tree.insert(root, number)
     avl_tree.postorder(root)
